93cb128e26a08eb0258243d6025b6d79c9f9f1bd
hkui0791-cyber <hkui0791@gmail.com>
2026-06-23 20:06:13 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The Ashan Bot",
+        name: "An Aᴇᴛʜᴇʀɪs Product",
         // Activity type number (0 = Playing).
         type: 0,
       },
